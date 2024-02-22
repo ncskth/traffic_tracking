15958856437eb32fed368cc4d28b034b4e916067
klownfish <github@clownfish.xyz>
2024-02-22 15:42:53 +0100

    rock3a config

--- interface/config.py
+++ interface/config.py
@@ -1,7 +1,8 @@
-RECORD_DIR = "/tmp/recordings/"
-TMP_DIR = "/tmp/recordings/"
+RECORD_DIR = "/mnt/nvme/recordings"
+TMP_DIR = "/mnt/nvme/tmp/"
 EVENT_SNAPSHOT_FILE_NAME = "event_snapshot.bmp"
 VIDEO_SNAPSHOT_FILE_NAME = "video_snapshot.jpg"
-PORT = 8090
-DEBUG = False
 SERIAL_PORT="/dev/ttyUSB0"
+PORT = 80
+DEBUG = False
+
--- recorder/config.h
+++ recorder/config.h
@@ -1,5 +1,5 @@
-#define TMP_DIR "/tmp/"
-#define VIDEO_CAMERA "/dev/video2"
+#define TMP_DIR "/mnt/nvme/tmp/"
+#define VIDEO_CAMERA "/dev/video0"
 
 #define EVENT_SNAPSHOT_FILE_NAME "event_snapshot.bmp"
 #define VIDEO_SNAPSHOT_FILE_NAME "video_snapshot.jpg"
